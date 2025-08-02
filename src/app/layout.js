7b2a5de953bf1e8071feed1677f33a700630e87e
Carlos Sanchez <carlossc999@gmail.com>
2025-08-02 10:34:38 +0100

    Added vercel speed instights and analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import Navbar from "../components/NavBar";
 import Footer from "../sections/home/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: "Casancam Website & Portfolio",
@@ -12,6 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="antialiased">
+        <SpeedInsights />
+        <Analytics />
         <Navbar />
         {children}
         <Footer />
